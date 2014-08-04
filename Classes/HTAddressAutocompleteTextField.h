ed53ef2f9df12cb59ef45cbd051e47b527b0967e
Kav Latiolais <kav@liffft.com>
2014-08-04 14:12:27 -0400

    Moved manager code into Address completion subclass also address cache is now case insensitive as well.

---
+++ Classes/HTAddressAutocompleteTextField.h
@@ -0,0 +1,13 @@
+//
+//  HTAddressAutocompleteTextField.h
+//  HTTextFieldAutocompletionExample
+//
+//  Created by Kav Latiolais on 8/4/14.
+//  Copyright (c) 2014 Hotel Tonight. All rights reserved.
+//
+
+#import "HTAutocompleteTextField.h"
+
+@interface HTAddressAutocompleteTextField : HTAutocompleteTextField <HTAutocompleteDataSource>
+@property (nonatomic, strong) NSString *postalCode;
+@end
